17d3afb03be1f1bd3b858fd6ee917f061b90fabd
datalater <the7mincheol@gmail.com>
2022-08-22 22:42:50 +0900

    feat: Person 랜덤 움직임 추가

--- src/objects/person.ts
+++ src/objects/person.ts
@@ -1,11 +1,6 @@
 import { GameObject } from '.';
 import { drawLayer1 } from '../canvas';
-import {
-  degreeToRadian,
-  getRandomInt,
-  getRandomInteger,
-  getTimings,
-} from '../utils';
+import { degreeToRadian, getRandomInteger, getTimings } from '../utils';
 
 type PersonState = {
   position: {
@@ -15,7 +10,11 @@ type PersonState = {
   };
 };
 
-const DISTANCE_PER_SECOND = 80;
+const PADDING = 10;
+const SPEED_MAX_MULTIPLE = 0.4;
+const SPEED_MIN_MULTIPLE = -1;
+
+const DISTANCE_PER_SECOND = 120;
 const FRAME_RATE_PER_SECOND = 60;
 
 /**
@@ -23,12 +22,9 @@ const FRAME_RATE_PER_SECOND = 60;
  */
 const DEFAULT_SPEED = DISTANCE_PER_SECOND / FRAME_RATE_PER_SECOND;
 
-const MOVE_PATTERNS = ['random', 'speedDown', 'speedUp', 'gentle', 'steep'];
-
 export default class Person implements GameObject, PersonState {
   position: { x: number; y: number; z: number };
   move: {
-    speed: number;
     position: {
       x: number;
       y: number;
@@ -43,14 +39,17 @@ export default class Person implements GameObject, PersonState {
   intervals: number[];
   randomX: number;
   randomY: number;
+  isMoving: boolean;
+  defaultSpeed: number;
 
-  constructor() {}
+  constructor(defaultSpeed: number = DEFAULT_SPEED) {
+    this.defaultSpeed = defaultSpeed;
+  }
 
   init = (state: PersonState) => {
     const { position } = state;
     this.position = position;
     this.move = {
-      speed: 3000,
       position: {
         x: 200,
         y: 200,
@@ -70,12 +69,12 @@ export default class Person implements GameObject, PersonState {
       10_000,
     ];
 
-    this.randomX = getRandomInteger(-1, 0.6);
-    this.randomY = getRandomInteger(-1, 0.6);
+    this.randomX = getRandomInteger(SPEED_MIN_MULTIPLE, SPEED_MAX_MULTIPLE);
+    this.randomY = getRandomInteger(SPEED_MIN_MULTIPLE, SPEED_MAX_MULTIPLE);
 
-    setInterval(() => {
-      this.randomX = getRandomInteger(-1, 0.6);
-      this.randomY = getRandomInteger(-1, 0.6);
+    const changeRandomValues = () => {
+      this.randomX = getRandomInteger(SPEED_MIN_MULTIPLE, SPEED_MAX_MULTIPLE);
+      this.randomY = getRandomInteger(SPEED_MIN_MULTIPLE, SPEED_MAX_MULTIPLE);
 
       this.intervals = [
         getRandomInteger(1000, 3000),
@@ -83,65 +82,80 @@ export default class Person implements GameObject, PersonState {
         getRandomInteger(6000, 8000),
         10_000,
       ];
-    }, 10_000);
+    };
+
+    setInterval(changeRandomValues, 10_000);
+
+    const changeDirection = () => {
+      getRandomInteger(-1, 1) > 0 && this.#changeDirectionX();
+      getRandomInteger(-1, 1) > 0 && this.#changeDirectionY();
+    };
+
+    setInterval(changeDirection, 8_000);
   };
 
   update = (time: DOMHighResTimeStamp) => {
     this.draw(time);
 
     if (time % 10_000 < this.intervals[0]) {
-      this.randomMove();
+      this.#randomMove();
       return;
     }
     if (time % 10_000 < this.intervals[1]) {
-      this.gentle();
+      this.#gentle();
       return;
     }
     if (time % 10_000 < this.intervals[2]) {
       const start = this.intervals[1] + Math.floor(time / 10_000) * 10_000;
       const duration = this.intervals[2] - this.intervals[1];
-      this.speedDown(getTimings({ time, start, duration }).progress);
+      this.#speedDown(getTimings({ time, start, duration }).progress);
       return;
     }
     if (time % 10_000 < this.intervals[3]) {
       const start = this.intervals[2] + Math.floor(time / 10_000) * 10_000;
       const duration = this.intervals[3] - this.intervals[2];
-      this.speedUp(getTimings({ time, start, duration }).progress);
+      this.#speedUp(getTimings({ time, start, duration }).progress);
       return;
     }
   };
 
-  randomMove = () => {
-    this.#moveX(DEFAULT_SPEED * (1 + this.randomX));
-    this.#moveY(DEFAULT_SPEED * (1 + this.randomY));
+  #randomMove = () => {
+    this.isMoving = true;
+    this.#moveX(this.defaultSpeed * (1 + this.randomX));
+    this.#moveY(this.defaultSpeed * (1 + this.randomY));
 
     this.#stayInViewport();
   };
 
-  gentle = () => {
-    this.#moveX(DEFAULT_SPEED);
-    this.#moveY(DEFAULT_SPEED * 0.2);
+  #gentle = () => {
+    this.isMoving = true;
+    this.#moveX(this.defaultSpeed);
+    this.#moveY(this.defaultSpeed * 0.2);
 
     this.#stayInViewport();
   };
 
-  steep = () => {
-    this.#moveX(DEFAULT_SPEED);
-    this.#moveY(DEFAULT_SPEED * 1.2);
+  #steep = () => {
+    this.isMoving = true;
+    this.#moveX(this.defaultSpeed);
+    this.#moveY(this.defaultSpeed * 1.2);
 
     this.#stayInViewport();
   };
 
-  speedDown = (progress?: number) => {
-    this.#moveX(DEFAULT_SPEED * (1 - progress));
-    this.#moveY(DEFAULT_SPEED * (1 - progress));
+  #speedDown = (progress?: number) => {
+    this.isMoving = true;
+    if (progress === 1) return (this.isMoving = false);
+    this.#moveX(this.defaultSpeed * (1 - progress));
+    this.#moveY(this.defaultSpeed * (1 - progress));
 
     this.#stayInViewport();
   };
 
-  speedUp = (progress?: number) => {
-    this.#moveX(DEFAULT_SPEED * progress);
-    this.#moveY(DEFAULT_SPEED * progress);
+  #speedUp = (progress?: number) => {
+    this.isMoving = true;
+    this.#moveX(this.defaultSpeed * progress);
+    this.#moveY(this.defaultSpeed * progress);
 
     this.#stayInViewport();
   };
@@ -152,142 +166,126 @@ export default class Person implements GameObject, PersonState {
 
   draw = (time: number) => {
     drawLayer1((context, canvas) => {
-      const isMoving = false;
-      if (isMoving) {
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2 + -2,
-          canvas.height / 2 + 26 + Math.sin(time / 64) * 2,
-        );
-        context.rotate(degreeToRadian(Math.sin(time / 128) * -8));
-        this.#drawArm(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2,
-          canvas.height / 2 + 24 + Math.sin(time / 128) * 2,
-        );
-        this.#drawUpperBody(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2,
-          canvas.height / 2 + 63 + Math.sin(time / 128) * 2,
-        );
-        this.#drawLowerBody(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1.1,
-          canvas.width / 2,
-          canvas.height / 2 + 70,
-        );
-        context.rotate(degreeToRadian(-2 + Math.sin(time / 128) * 14));
-        this.#drawLeg(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1.1,
-          canvas.width / 2 + 14,
-          canvas.height / 2 + 70,
-        );
-        context.rotate(degreeToRadian(5 + Math.sin(time / 128) * -14));
-        this.#drawLeg(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2,
-          canvas.height / 2 + Math.sin(time / 128) * 2,
-        );
-        this.#drawHead(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2 + 18,
-          canvas.height / 2 + 26 + Math.sin(time / 64) * 2,
-        );
-        context.rotate(degreeToRadian(Math.sin(time / 128) * 8));
-        this.#drawArm(context);
+      const { x, y, z } = this.position;
+
+      if (this.isMoving) {
+        this.#drawMovement(context, canvas, time, this.position);
       } else {
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2 - 4,
-          canvas.height / 2 + 26 + Math.sin(time / 128) * 1,
-        );
-        this.#drawArm(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2,
-          canvas.height / 2 + 24 + Math.sin(time / 128) * 1,
-        );
-        this.#drawUpperBody(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2,
-          canvas.height / 2 + 62,
-        );
-        this.#drawLowerBody(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2,
-          canvas.height / 2 + 73,
-        );
-        this.#drawLeg(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2 + 14,
-          canvas.height / 2 + 73,
-        );
-        this.#drawLeg(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2 + 20,
-          canvas.height / 2 + 26 + Math.sin(time / 128) * 1,
-        );
-        this.#drawArm(context);
-        context.setTransform(
-          1,
-          0,
-          0,
-          1,
-          canvas.width / 2,
-          canvas.height / 2 + Math.sin(time / 128) * 2,
-        );
-        this.#drawHead(context);
+        this.#drawIdle(context, canvas, time, this.position);
       }
     });
   };
+
+  #drawIdle(
+    context: CanvasRenderingContext2D,
+    canvas: HTMLCanvasElement,
+    time: number,
+    position: { x: number; y: number },
+  ) {
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x - 4,
+      position.y + 26 + Math.sin(time / 128) * 1,
+    );
+    this.#drawArm(context);
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x,
+      position.y + 24 + Math.sin(time / 128) * 1,
+    );
+    this.#drawUpperBody(context);
+    context.setTransform(1, 0, 0, 1, position.x, position.y + 62);
+    this.#drawLowerBody(context);
+    context.setTransform(1, 0, 0, 1, position.x, position.y + 73);
+    this.#drawLeg(context);
+    context.setTransform(1, 0, 0, 1, position.x + 14, position.y + 73);
+    this.#drawLeg(context);
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x + 20,
+      position.y + 26 + Math.sin(time / 128) * 1,
+    );
+    this.#drawArm(context);
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x,
+      position.y + Math.sin(time / 128) * 2,
+    );
+    this.#drawHead(context);
+  }
+
+  #drawMovement(
+    context: CanvasRenderingContext2D,
+    canvas: HTMLCanvasElement,
+    time: number,
+    position: { x: number; y: number },
+  ) {
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x + -2,
+      position.y + 26 + Math.sin(time / 64) * 2,
+    );
+    context.rotate(degreeToRadian(Math.sin(time / 128) * -8));
+    this.#drawArm(context);
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x,
+      position.y + 24 + Math.sin(time / 128) * 2,
+    );
+    this.#drawUpperBody(context);
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x,
+      position.y + 63 + Math.sin(time / 128) * 2,
+    );
+    this.#drawLowerBody(context);
+    context.setTransform(1, 0, 0, 1.1, position.x, position.y + 70);
+    context.rotate(degreeToRadian(-2 + Math.sin(time / 128) * 14));
+    this.#drawLeg(context);
+    context.setTransform(1, 0, 0, 1.1, position.x + 14, position.y + 70);
+    context.rotate(degreeToRadian(5 + Math.sin(time / 128) * -14));
+    this.#drawLeg(context);
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x,
+      position.y + Math.sin(time / 128) * 2,
+    );
+    this.#drawHead(context);
+    context.setTransform(
+      1,
+      0,
+      0,
+      1,
+      position.x + 18,
+      position.y + 26 + Math.sin(time / 64) * 2,
+    );
+    context.rotate(degreeToRadian(Math.sin(time / 128) * 8));
+    this.#drawArm(context);
+  }
 
   #drawHead = (context: CanvasRenderingContext2D) => {
     const skinColor = '#ffefdb';
@@ -342,11 +340,25 @@ export default class Person implements GameObject, PersonState {
   }
 
   #stayInViewport() {
-    if (this.position.x < 0 || this.position.x >= document.body.clientWidth) {
-      this.move.direction.x = (-1 * this.move.direction.x) as -1 | 1;
+    if (
+      this.position.x < PADDING * -2 ||
+      this.position.x >= document.body.clientWidth
+    ) {
+      this.#changeDirectionX();
     }
-    if (this.position.y < 0 || this.position.y >= document.body.clientHeight) {
-      this.move.direction.y = (-1 * this.move.direction.y) as -1 | 1;
+    if (
+      this.position.y < PADDING * -1 ||
+      this.position.y >= document.body.clientHeight
+    ) {
+      this.#changeDirectionY();
     }
+  }
+
+  #changeDirectionY() {
+    this.move.direction.y = (-1 * this.move.direction.y) as -1 | 1;
+  }
+
+  #changeDirectionX() {
+    this.move.direction.x = (-1 * this.move.direction.x) as -1 | 1;
   }
 }
