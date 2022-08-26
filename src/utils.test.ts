b5c69e5a6f0b5c59832b0c0a32336b4df5621618
neokkk <seven3126@gmail.com>
2022-08-26 18:52:21 +0900

    --amend

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,4 +1,4 @@
-import { getStringifiedStyle, camelToKebab } from './utils';
+import { camelToKebab } from './utils';
 
 describe('camelToKebab', () => {
   it('kebab-case로 문자열을 변환한다.', () => {
@@ -18,27 +18,6 @@ describe('camelToKebab', () => {
   it('인자를 넘기지 않으면 빈 문자를 반환한다.', () => {
     expect(
       camelToKebab(undefined),
-    ).toBe('');
-  });
-});
-
-describe('getStringifiedStyle', () => {
-  it('styleObject를 인자로 넘겼을 때 적절한 문자열을 반환한다.', () => {
-    const styleObj = {
-      width: '200px',
-      height: 'auto',
-      overflow: 'hidden',
-      zIndex: '1',
-    };
-
-    expect(
-      getStringifiedStyle(styleObj),
-    ).toBe('width:200px;height:auto;overflow:hidden;z-index:1');
-  });
-
-  it('인자를 넘기지 않으면 빈 문자를 반환한다.', () => {
-    expect(
-      getStringifiedStyle(),
     ).toBe('');
   });
 });
