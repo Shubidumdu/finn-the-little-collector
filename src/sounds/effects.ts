6c1dbb6800e0def5ea873f61e280055e310cf9ff
Alan Seo <alan@grepp.co>
2022-09-05 22:12:38 +0900

    feat: person 눌렀을 떄의 사운드 이펙트 추가 & 오류 수정

--- src/sounds/effects.ts
+++ src/sounds/effects.ts
@@ -1,18 +1,33 @@
 import store from '../store';
 import { zzfx } from './libs/zzfx';
 
-type SoundType = 'pick';
+type SoundType = 'pick' | 'correct' | 'wrong';
 
 const playEffectSound = (type: SoundType) => {
   if (!store.isSoundOn) return;
   switch (type) {
     case 'pick':
       soundPick();
+      break;
+    case 'correct':
+      soundCorrect();
+      break;
+    case 'wrong':
+      soundWrong();
+      break;
   }
 };
 
 const soundPick = () => {
   zzfx(...[.3,,1384,.02,.01,0,,1.36,8.5,,,,,,15,,,,.01]);
 };
+
+const soundCorrect = () => {
+  zzfx(...[.6,,128,.01,.01,.09,,1.46,-14,,,,,,,.1,.01,.43,.04]);
+}
+
+const soundWrong = () => {
+  zzfx(...[.4,,455,,.07,.07,,.09,,-0.2,,,.04,.8,33,.4,.29,.85,.09]);
+}
 
 export default playEffectSound;
